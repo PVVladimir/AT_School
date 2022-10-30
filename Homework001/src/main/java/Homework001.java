a26a6ea090e2c2a72c1c953b5bbfb6b725acc9ae
vfilipkin <vladimir.filipkin@vsegda-da.com>
2022-10-30 23:58:43 +0300

    Homework for lesson 1

---
+++ Homework001/src/main/java/Homework001.java
@@ -0,0 +1,29 @@
+public class Homework001 {
+
+    private static double squareArea(double side){
+        return Math.pow(side, 2);
+    }
+
+    private static double circleArea(double radius){
+        return Math.PI * Math.pow(radius, 2);
+    }
+
+    private static double triangleArea(double sideA, double sideB, double sideC){
+        double semiPerimeter = (sideA + sideB + sideC) / 2.0;
+        return Math.sqrt(semiPerimeter * (semiPerimeter - sideA) * (semiPerimeter - sideB) * (semiPerimeter - sideC));
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello World, я ${username}");
+        double squareSide = 2.7;
+        System.out.println("Площадь квадрата с стороной " + squareSide + " равна " + squareArea(squareSide));
+        double circleRadius = 7.3;
+        System.out.println("Площадь круга с радиусом " + circleRadius + " равна " + circleArea(circleRadius));
+        double triangleSideA = 3.3;
+        double triangleSideB = 4.4;
+        double triangleSideC = 5.5;
+        System.out.println("Площадь треугольника с сторонами "
+                + triangleSideA + ", " + triangleSideB + ", " + triangleSideC + ", "
+                + " равна " + triangleArea(triangleSideA, triangleSideB, triangleSideC));
+    }
+}
