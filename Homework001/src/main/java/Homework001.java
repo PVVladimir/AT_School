502d95f0032b9b44d1e4e522d82c9660896bc3da
vfilipkin <vladimir.filipkin@vsegda-da.com>
2022-10-31 00:30:38 +0300

    Homework for lesson 1

--- Homework001/src/main/java/Homework001.java
+++ Homework001/src/main/java/Homework001.java
@@ -1,3 +1,7 @@
+/*
+
+ * Homework for lesson 1
+ */
 public class Homework001 {
 
     private static double squareArea(double side){
@@ -10,15 +14,18 @@ public class Homework001 {
 
     private static double triangleArea(double sideA, double sideB, double sideC){
         double semiPerimeter = (sideA + sideB + sideC) / 2.0;
-        return Math.sqrt(semiPerimeter * (semiPerimeter - sideA) * (semiPerimeter - sideB) * (semiPerimeter - sideC));
+        return Math.sqrt(semiPerimeter * (semiPerimeter - sideA) * (semiPerimeter - sideB)
+                * (semiPerimeter - sideC));
     }
 
     public static void main(String[] args) {
         System.out.println("Hello World, я ${username}");
         double squareSide = 2.7;
-        System.out.println("Площадь квадрата с стороной " + squareSide + " равна " + squareArea(squareSide));
+        System.out.println("Площадь квадрата с стороной " + squareSide + " равна "
+                + squareArea(squareSide));
         double circleRadius = 7.3;
-        System.out.println("Площадь круга с радиусом " + circleRadius + " равна " + circleArea(circleRadius));
+        System.out.println("Площадь круга с радиусом " + circleRadius + " равна "
+                + circleArea(circleRadius));
         double triangleSideA = 3.3;
         double triangleSideB = 4.4;
         double triangleSideC = 5.5;
