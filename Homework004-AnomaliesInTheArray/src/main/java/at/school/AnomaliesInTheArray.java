e8faa61d4fd2ba0f57f458c08195629006738f50
vfilipkin <vladimir.filipkin@vsegda-da.com>
2022-11-16 08:33:22 +0300

    Homework for lesson 4. Array and if tasks

--- Homework004-AnomaliesInTheArray/src/main/java/at/school/AnomaliesInTheArray.java
+++ Homework004-AnomaliesInTheArray/src/main/java/at/school/AnomaliesInTheArray.java
@@ -1,36 +1,41 @@
 package at.school;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Random;
 
 public class AnomaliesInTheArray {
     public static void main(String[] args) {
         ArrayList<Integer> arrayWithAnomalies = new ArrayList<>();
         final int COUNT_ELEMENTS = 100;
-        int startIndex = 0;
         Random random = new Random();
         for (int i = 0; i < COUNT_ELEMENTS; i++) {
+//            arrayWithAnomalies.add(8);
+//            arrayWithAnomalies.add(i);
+//            arrayWithAnomalies.add(100 - i);
             arrayWithAnomalies.add(random.nextInt(100));
         }
-        boolean isIncrease;
-        System.out.println(arrayWithAnomalies.get(startIndex));
-        do {
-            isIncrease = arrayWithAnomalies.get(startIndex) < arrayWithAnomalies.get(startIndex + 1);
-            startIndex++;
-            if (arrayWithAnomalies.get(startIndex - 1) == arrayWithAnomalies.get(startIndex)) {
-                System.out.println(arrayWithAnomalies.get(startIndex) + "<---- аномальный элемент");
-            } else {
-                System.out.println(arrayWithAnomalies.get(startIndex));
-            }
-        } while (arrayWithAnomalies.get(startIndex - 1) == arrayWithAnomalies.get(startIndex) &&
-                startIndex < COUNT_ELEMENTS - 1);
-        for (int i = startIndex + 1; i < COUNT_ELEMENTS; i++) {
-            if (isIncrease && arrayWithAnomalies.get(i - 1) >= arrayWithAnomalies.get(i) ||
-                    !isIncrease && arrayWithAnomalies.get(i - 1) <= arrayWithAnomalies.get(i)) {
-                isIncrease = !isIncrease;
+        System.out.println(arrayWithAnomalies.get(0));
+        boolean isIncrease = arrayWithAnomalies.get(0) < arrayWithAnomalies.get(1);
+        boolean isDecrease = arrayWithAnomalies.get(0) > arrayWithAnomalies.get(1);
+        for (int i = 1; i < COUNT_ELEMENTS; i++) {
+            if (arrayWithAnomalies.get(i - 1) == arrayWithAnomalies.get(i)) {
                 System.out.println(arrayWithAnomalies.get(i) + "<---- аномальный элемент");
+                isIncrease = isDecrease = false;
             } else {
-                System.out.println(arrayWithAnomalies.get(i));
+                if (!isIncrease && !isDecrease){
+                    isIncrease = arrayWithAnomalies.get(i - 1) < arrayWithAnomalies.get(i);
+                    isDecrease = arrayWithAnomalies.get(i - 1) > arrayWithAnomalies.get(i);
+                    System.out.println(arrayWithAnomalies.get(i));
+                } else {
+                    if (isIncrease && arrayWithAnomalies.get(i - 1) < arrayWithAnomalies.get(i) ||
+                            isDecrease && arrayWithAnomalies.get(i - 1) > arrayWithAnomalies.get(i)) {
+                        System.out.println(arrayWithAnomalies.get(i));
+                    } else {
+                        System.out.println(arrayWithAnomalies.get(i) + "<---- аномальный элемент");
+                        isIncrease = isDecrease = false;
+                    }
+                }
             }
         }
     }
